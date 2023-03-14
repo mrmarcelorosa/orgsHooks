980d03d5417a0f26e3c5d72d8d579fc35204f147
Marcelo Rosa <vyctor.rosa14@gmail.com>
2023-03-13 22:15:35 -0300

    Carrega mock no compoente topo

--- src/telas/Home/components/Topo.tsx
+++ src/telas/Home/components/Topo.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
-
 import logo from "../../../assets/logo.png";
+import { carregaTopo } from "../../../services/carregaDados";
 
-export default function Topo() {
-    return <View style={styles.topo}>
+class Topo extends React.Component {
+
+    atualizaTopo(){
+        const retorno = carregaTopo();
+        console.log(retorno)
+    }
+
+    componentDidMount(): void {
+        this.atualizaTopo(); 
+    }
+
+    render (){
+        return <View style={styles.topo}>
         <Image source={logo} style={styles.imagem}/>
         <Text style={styles.boasVindas}>Olá Marcelo</Text>
         <Text style={styles.legenda}>Encontre os melhores produtores</Text>
     </View>
+    }
+    
 }
 
 const styles = StyleSheet.create({
@@ -32,4 +45,6 @@ const styles = StyleSheet.create({
         lineHeight: 26
     }
 
-})
+});
+
+export default Topo;
